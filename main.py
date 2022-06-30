8e31c0184af3c04db3b1809a9e97abc94337958c
bamcasa <combeesang@gmail.com>
2022-06-30 23:46:10 +0900

    add remove

--- main.py
+++ main.py
@@ -11,23 +11,26 @@ data = {"타이틀": ["티몬 몬소리체",
                "윤고딕",
                "맑은 고딕",
                "스포카 한 산스"],
-        "포스터" : ["양진체 - 타이포 그래피 ( 유료화됨)",
-                 "쿠키런",
-                 "검은고딕",
-                 "말싸미815",
-                 "116수박화체",
-                 "헤드라인",
-                 "hs두꺼비체",
-                 "hs봄바람체",
-                 "베스킨라빈스b"]
+        "포스터": ["양진체 - 타이포 그래피 ( 유료화됨)",
+                "쿠키런",
+                "검은고딕",
+                "말싸미815",
+                "116수박화체",
+                "헤드라인",
+                "hs두꺼비체",
+                "hs봄바람체",
+                "베스킨라빈스b"]
         }
-input_data = input("입력 : ")
-result = set()
 
-result = result | set(data[input_data])
-print(result)
+result = set()
 
 while True:
     input_data = input("입력 : ")
-    result = result | set(data[input_data])
+    sliced_input = input_data.split(" ")
+    index = sliced_input[0]
+    act = sliced_input[1]
+    if act == 0 or act == "추가":
+        result = result | set(data[index])
+    elif act == 1 or act == "제거":
+        result = result - set(data[index])
     print(result)
